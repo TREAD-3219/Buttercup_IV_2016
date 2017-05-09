3b8dd7088892c5944522ff37119effbfa486604f
Team TREAD developer login for Github <tread3219@gmail.com>
2017-05-09 16:11:14 -0700

    Added stickDrive, added motors to robotmap

---
+++ src/org/usfirst/frc3219/ButtercupIV/commands/StickDrive.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc3219.ButtercupIV.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class StickDrive extends Command {
+
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+}
